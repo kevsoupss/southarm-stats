3f81807a67989fed322b0d162f4918033882623f
Kevin Lei <kevinleimc@gmail.com>
2025-06-19 19:58:08 -0700

    first commit

---
+++ src/main/java/com/southarmsite/backend/SoutharmStatsApplication.java
@@ -0,0 +1,13 @@
+package com.southarmsite.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoutharmStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoutharmStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/southarmsite/backend/SoutharmStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southarmsite.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoutharmStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
